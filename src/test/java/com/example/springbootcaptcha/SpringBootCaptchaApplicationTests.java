424a7b96528e8fa0bf013b13e1e4bc0334cf13c3
Kemal Yuksel <kemalyksell98@gmail.com>
2023-01-09 16:07:29 +0300

    Initial commit

---
+++ src/test/java/com/example/springbootcaptcha/SpringBootCaptchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcaptcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCaptchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
