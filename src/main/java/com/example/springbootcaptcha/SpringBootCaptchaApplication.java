49d29b9647c1183b0bf2d49a2ed5e814c1c3bf9b
Kemal Yuksel <kemalyksell98@gmail.com>
2023-01-09 16:09:09 +0300

    DefaultKaptcha dependency add

--- src/main/java/com/example/springbootcaptcha/SpringBootCaptchaApplication.java
+++ src/main/java/com/example/springbootcaptcha/SpringBootCaptchaApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootCaptchaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCaptchaApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootCaptchaApplication.class, args);
+    }
 
 }
